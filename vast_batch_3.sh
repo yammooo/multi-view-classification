0428f37af1bc96682a2c0f5cca269edb91093672
Gianmaria Frigo <gianmariafrigo3@gmail.com>
2025-03-26 09:05:05 +0100

    increase sleep duration to 30 minutes in vast_batch_3.sh; add vastai to requirements in vast_req.txt

--- vast_batch_3.sh
+++ vast_batch_3.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=2 python multi-view-classification/vast_train_11_score.py
 CUDA_VISIBLE_DEVICES=2 python multi-view-classification/vast_train_13_score.py
 
 # Wait 15 minutes (15*60 = 900 seconds)
-sleep 900
+sleep 1800
 
 # Call the Vast API to stop the instance
 vastai stop instance 19024669
