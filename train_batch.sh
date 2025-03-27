f4abf6fbd9058b3a21fbe30d3d0c8f6b208f3370
Gianmaria Frigo <gianmariafrigo3@gmail.com>
2025-03-27 16:27:21 +0100

    add cache cleanup in training script for multiple backbone models

--- train_batch.sh
+++ train_batch.sh
@@ -1,8 +1,14 @@
 python multi-view-classification/train.py '{"backbone_model": "convnextsmall", "fusion_strategy": "score", "fusion_depth": null, "next_start_layer": null, "fusion_method": "sum", "freeze_config": {"freeze_blocks": ["stem", "stage_0"]}}'
 
+rm -rf .cache
+
 python multi-view-classification/train.py '{"backbone_model": "vitb16", "fusion_strategy": "score", "fusion_depth": null, "next_start_layer": null, "fusion_method": "sum", "freeze_config": {"freeze_blocks": []}}'
 
+rm -rf .cache
+
 python multi-view-classification/train.py '{"backbone_model": "resnet152", "fusion_strategy": "score", "fusion_depth": null, "next_start_layer": null, "fusion_method": "sum", "freeze_config": {"freeze_blocks": ["conv1", "conv2", "conv3"]}}'
+
+rm -rf .cache
 
 python multi-view-classification/train.py '{"backbone_model": "efficientnetb0", "fusion_strategy": "score", "fusion_depth": null, "next_start_layer": null, "fusion_method": "sum", "freeze_config": {"freeze_blocks": ["stem", "block1", "block2"]}}'
 
