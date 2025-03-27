01a12c56818e35c59a7caa6d4c6ce4f5225a7a1b
Gianmaria Frigo <gianmariafrigo3@gmail.com>
2025-03-27 16:29:28 +0100

    update instance ID in train_batch.sh for stopping vast.ai instance

--- train_batch.sh
+++ train_batch.sh
@@ -12,4 +12,4 @@ rm -rf .cache
 
 python multi-view-classification/train.py '{"backbone_model": "efficientnetb0", "fusion_strategy": "score", "fusion_depth": null, "next_start_layer": null, "fusion_method": "sum", "freeze_config": {"freeze_blocks": ["stem", "block1", "block2"]}}'
 
-vastai stop instance 19062284
+vastai stop instance 19062359
